20dd026097ab04eb83f24728bfc0d8f2b8e5f11e
ecchsu <40111160+ecchsu@users.noreply.github.com>
2025-10-15 10:50:13 +0800

    Update EvaAdapterModuleInstrumentation.java

--- src/main/java/com/eva/arex/plugin/EvaAdapterModuleInstrumentation.java
+++ src/main/java/com/eva/arex/plugin/EvaAdapterModuleInstrumentation.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
- * Main entry point for AREX plugin to instrument com.eva.adapter package
+ * Main entry point for AREX plugin to instrument com.eva.adapter packages
  * This class will be discovered by AREX agent via SPI
  */
 @AutoService(ModuleInstrumentation.class)
@@ -16,14 +16,16 @@ public class EvaAdapterModuleInstrumentation extends ModuleInstrumentation {
 
     public EvaAdapterModuleInstrumentation() {
         super("plugin-eva-adapter");
+        //Log when plugin is constructed
+        System.out.println("[AREX] plugin-eva-adapter called!!");
     }
 
     @Override
     public List<TypeInstrumentation> instrumentationTypes() {
+        System.out.println("[AREX] plugin-eva-adapter instrumentationTypes() called!!");
         // Return all instrumentation implementations
         return Arrays.asList(
-            new EvaAdapterPackageInstrumentation(),
-            new UseObjectPhaseAnnotationInstrumentation()
+            new EvaAdapterPackageInstrumentation()
         );
     }
 }
